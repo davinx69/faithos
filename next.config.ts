71aea555c173cfbf167b83697022262ea6d855fc
Daniel Vincent <danielvincent619@gmail.com>
2026-05-22 22:22:54 +0100

    Fix Content Security Policy for Supabase

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "script-src 'self' 'unsafe-eval' 'unsafe-inline' https:; connect-src 'self' https://*.supabase.co wss://*.supabase.co https:;",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
